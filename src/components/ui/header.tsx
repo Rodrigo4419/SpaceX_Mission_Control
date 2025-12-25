1f2ec486d8f35c82a2ed38d389aaf4755c2cc375
rodrigo4419 <cobronio@gmail.com>
2025-12-25 15:03:58 -0600

    theme and drawer changes

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,8 +1,8 @@
-"use client"
+"use client";
 import { usePathname } from "next/navigation";
 import { ChevronRightIcon, MenuIcon } from "@/components/svg";
 import Drawer from "@/components/layout/Drawer";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const titlesMap: Record<string, string> = {
   "/": "Home",
@@ -11,12 +11,15 @@ const titlesMap: Record<string, string> = {
   "/profile": "My Profile",
 };
 
-
 export default function HeaderComponent() {
+  const pathname = usePathname();
+  const title = titlesMap[pathname] ?? "Page";
+  const [showDrawer, setShowDrawer] = useState(false);
 
-const pathname = usePathname();
-const title = titlesMap[pathname] ?? "Page";
-const [showDrawer, setShowDrawer] =useState(false)
+  useEffect(() => {
+    setShowDrawer(false);
+  }, [pathname]);
+
   return (
     <header className="max-w-425 m-auto fixed top-0 left-0 w-dvw h-24 p-4 bg-zinc-900 border-b border-b-zinc-800 z-10 sm:relative sm:w-auto sm:h-fit sm:p-0 sm:bg-transparent sm:border-b-0 sm:border-b-transparent">
       <div className="flex gap-x-1.5 items-center text-sm text-zinc-400 font-medium mb-5">
@@ -26,12 +29,12 @@ const [showDrawer, setShowDrawer] =useState(false)
       </div>
       <h2 className="font-bold">Overview</h2>
       <button
-           onClick={() => setShowDrawer(!showDrawer)}
-          className="absolute bottom-4 right-4 h-8 w-8 rounded-md border border-zinc-700 flex items-center justify-center disabled:text-zinc-800 disabled:pointer-events-none sm:hidden"
-        >
-          <MenuIcon width="1rem" height="1rem" />
-        </button>
-        <Drawer show={showDrawer} setShow={setShowDrawer}/>
+        onClick={() => setShowDrawer(!showDrawer)}
+        className="absolute bottom-4 right-4 h-8 w-8 rounded-md border border-zinc-700 flex items-center justify-center disabled:text-zinc-800 disabled:pointer-events-none sm:hidden"
+      >
+        <MenuIcon width="1rem" height="1rem" />
+      </button>
+      <Drawer show={showDrawer} setShow={setShowDrawer} />
     </header>
   );
 }
